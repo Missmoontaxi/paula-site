361f6f46812b18f7a19912f5fcafece634a3b666
Paula McMahon <missmoontaxi@gmail.com>
2026-01-22 23:19:40 -0500

    shorter white space

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,7 +24,7 @@ export default function Home() {
   return (
     <>
       {/* Hero Section */}
-      <section className="relative min-h-[70vh] md:min-h-screen flex items-center justify-center overflow-hidden">
+      <section className="relative min-h-[60vh] md:min-h-screen flex items-center justify-center overflow-hidden">
         {/* Background Image */}
         <div className="absolute inset-0 z-0">
           <Image
